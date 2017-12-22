051811886315c116f5042e2178b8499ebe96cdf7
David Schmitz <david.schmitz.privat@gmail.com>
2017-12-22 13:09:01 +0100

    Refactored actions

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Employees from './components/employees'
 import Employee from './components/employee'
 import AwsConfig from './components/awsConfig'
-import fetch from 'isomorphic-fetch';
 import AWS from 'aws-sdk'
+import Actions from './actions'
 
 class App extends Component {
   constructor(props) {
@@ -24,13 +24,8 @@ class App extends Component {
   }
 
   onEmployeeSelected(employee) {
-    this.setState({
-      ...this.state,
-      selectedEmployee: employee
-    })
-
-    this.fetchContractForEmployee(employee)
-        .then(state => this.setState({ ...this.state, ...state }))
+    Actions.fetchContractForEmployee(employee)
+        .then(state => this.setState({ ...this.state, ...state, selectedEmployee: employee }))
         .catch(e => console.warn(e))
   }
 
@@ -41,62 +36,10 @@ class App extends Component {
     })
   }
 
-  fetchContractForEmployee(employee) {
-    if (process.env.NODE_ENV === 'development') {
-      return Promise.resolve({
-        loading: false,
-        contract: require('./mock/mockcontracts.js')[0],
-        errorMsg: null
-      })
-    }
-
-    fetch('https://17ndprt19g.execute-api.eu-central-1.amazonaws.com/Stage/employee/' + this.state.selectedEmployee.id + "/contract")
-      .then(response => {
-        if (response.status !== 200 && response.status !== 404) {
-          throw new Error("Bad response from server: " + response.status);
-        }
-        return response.status === 200 ? response.json() : null
-      })
-      .then(contracts => {
-        return ({
-            loading: false,
-            errorMsg: null,
-            contract: contracts.length > 0 ? contracts[0] : null
-        })
-      })
-      .catch(e => {
-         return ({ contract: null, loading: false, errorMsg: 'Cannot load contract! ' + (e.message || "Check logs for details") })
-      })
-  }
-
-  fetchEmployees() {
-    if (process.env.NODE_ENV === 'development') {
-      return Promise.resolve({
-        loading: false,
-        employees: require('./mock/mockemployees').users
-      })
-    }
-
-    return fetch('https://e3ixwum5ff.execute-api.eu-central-1.amazonaws.com/Stage/employee')
-            .then(response => {
-              if (response.status !== 200 && response.status !== 404) {
-                throw new Error("Bad response from server: " + response.status);
-              }
-              return response.status === 200 ? response.json() : null
-            })
-            .then(json => json.users)
-            .then(employees => employees.sort( (a,b) => a.name.last > b.name.last))
-            .then(employees => ({
-              loading: false,
-              employees: employees
-            }))
-            .catch(e => ({ employees: [], loading: false, errorMsg: 'Cannot load employees! ' + (e.message || "Check logs for details") }))
-  }
-
   componentDidMount() {
     this.setState({ ...this.state, loading: true })
-    this.fetchEmployees()
-        .then(state => this.setState({ ...this.state, ...state }))
+    Actions.fetchEmployees()
+        .then(state => this.setState({ ...this.state, ...state, loading: false }))
         .catch(e => console.warn(e))
   }
 
@@ -118,6 +61,9 @@ class App extends Component {
 
         <div>
           <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+        </div>
+        <div>
+          <small>You are using mock services <b>{process.env.REACT_APP_SERVICE === 'mock' ? 'yes' : 'no'}</b>.</small>
         </div>
       </div>
     );
--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,58 @@
-import C from './constants'
+import fetch from 'isomorphic-fetch';
 
-export const showContract = () =>
-(
-)
+const fetchContractForEmployee = (employee) => {
+  if (process.env.REACT_APP_SERVICE === 'mock') {
+    return Promise.resolve({
+      loading: false,
+      contract: require('./mock/mockcontracts.js')[0],
+      errorMsg: null
+    })
+  }
 
-export const showEmployee = (employeeId) =>
-(
-{
-  type: C.SHOW_EMPLOYEE,
-  employeeId
+  return fetch('https://17ndprt19g.execute-api.eu-central-1.amazonaws.com/Stage/employee/' + employee.id + "/contract")
+          .then(response => {
+            if (response.status !== 200 && response.status !== 404) {
+              throw new Error("Bad response from server: " + response.status);
+            }
+            return response.status === 200 ? response.json() : null
+          })
+          .then(contracts => {
+            return ({
+                loading: false,
+                errorMsg: null,
+                contract: contracts.length > 0 ? contracts[0] : null
+            })
+          })
+          .catch(e => {
+             return ({ contract: null, loading: false, errorMsg: 'Cannot load contract! ' + (e.message || "Check logs for details") })
+          })
 }
-)
 
-export const employeesActions = (dispatcher) =>
-({
+const fetchEmployees = () => {
+  if (process.env.REACT_APP_SERVICE === 'mock') {
+    return Promise.resolve({
+      loading: false,
+      employees: require('./mock/mockemployees').users
+    })
+  }
 
-})
+  return fetch('https://e3ixwum5ff.execute-api.eu-central-1.amazonaws.com/Stage/employee')
+          .then(response => {
+            if (response.status !== 200 && response.status !== 404) {
+              throw new Error("Bad response from server: " + response.status);
+            }
+            return response.status === 200 ? response.json() : null
+          })
+          .then(json => json.users)
+          .then(employees => employees.sort( (a,b) => a.name.last > b.name.last))
+          .then(employees => ({
+            loading: false,
+            employees: employees
+          }))
+          .catch(e => ({ employees: [], loading: false, errorMsg: 'Cannot load employees! ' + (e.message || "Check logs for details") }))
+}
 
-export const deleteEmployee = () =>
-(
-)
+export default {
+  fetchEmployees,
+  fetchContractForEmployee
+}
